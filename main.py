7abc6cc254283626e1486228a0282ab24530cd5c
Alexey Orlov <avorlov85@gmail.com>
2024-11-25 15:11:55 +0100

    added source code v.01 and readme.md

---
+++ main.py
@@ -0,0 +1,11 @@
+from fpdf import fpdf
+from bs4 import BeautifulSoup
+import requests
+url = 'https://yandex.com.am/weather/?lat=55.75581741&lon=37.61764526'
+response = requests.get(url)
+print(response)
+bs = BeautifulSoup(response.text, "lxml")
+print(bs)
+temp = bs.find("span", "temp__value temp__value_with-unit")
+print("XXXXXXXXXXXXXXXXXXXXX")
+print(temp)
